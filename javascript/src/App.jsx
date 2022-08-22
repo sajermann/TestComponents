e87d6c9ad015f54ca2db5278af30dd6b76adbfb9
Bruno Sajermann <sajermannbruno@gmail.com>
2022-08-22 08:44:34 -0300

    add new version sajermann/ui

--- javascript/src/App.jsx
+++ javascript/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { Button, Loading } from '@sajermann/ui-react';
+import { Button, Drawer } from '@sajermann/ui-react';
 import '@sajermann/ui-react/index.css';
 
 function App() {
@@ -9,6 +9,11 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
 	const [success, setSuccess] = useState(false);
 	const [failed, setFailed] = useState(false);
+
+	const [isOpenTop, setIsOpenTop] = useState(false);
+	const [isOpenLeft, setIsOpenLeft] = useState(false);
+	const [isOpenRight, setIsOpenRight] = useState(false);
+	const [isOpenFull, setIsOpenFull] = useState(false);
 
   function delay(delayMs) {
     return new Promise(resolve => {
@@ -21,32 +26,119 @@ function App() {
 		await delay(3000);
 		setIsLoading(false);
 		setSuccess(true);
+		await delay(1500);
+		setSuccess(false);
 	}
 
   return (
     <div className="App">
     <Button
 				disabled={isLoading}
-				colorStyle="Success"
+				colorStyle="Primary"
 				id="bruno"
 				type="button"
 				onClick={handle}
 				withFeedback={{
-					isLoading,
-					inSuccess: {
-						setSuccess,
+					loadingOptions:{
+						isLoading
+					},
+					successOptions: {
 						success,
 					},
-					inFailed: {
-						setFailed,
+					failedOptions: {
 						failed,
 					},
 				}}
 			>
 				Clique aqui
 			</Button>
-			<Loading />
-    </div>
+
+			<Button
+				colorStyle="Primary"
+				id="bruno"
+				type="button"
+				onClick={() => setIsOpenLeft(true)}
+			>
+				Open Left
+			</Button>
+			<Button
+				colorStyle="Secondary"
+				id="bruno"
+				type="button"
+				onClick={() => setIsOpenRight(true)}
+			>
+				Open Right
+			</Button>
+			<Button
+				colorStyle="Success"
+				id="bruno"
+				type="button"
+				onClick={() => setIsOpenFull(true)}
+			>
+				Open Full
+			</Button>
+			<Button
+				colorStyle="Warning"
+				id="bruno"
+				type="button"
+				onClick={() => setIsOpenTop(true)}
+			>
+				Open Top
+			</Button>
+			<Drawer
+				openFrom="left"
+				percentage={10}
+				isOpen={isOpenLeft}
+				setIsOpen={e => setIsOpenLeft(e)}
+			>
+				<div>left</div>
+			</Drawer>
+			<Drawer
+				openFrom="right"
+				percentage={30}
+				isOpen={isOpenRight}
+				setIsOpen={e => setIsOpenRight(e)}
+				disableBackdrop
+			>
+				<div>left</div>
+			</Drawer>
+			<Drawer
+				openFrom="bottom"
+				percentage={10}
+				isOpen={isOpenFull}
+				disableEsc
+				setIsOpen={e => setIsOpenFull(e)}
+			>
+				<div>
+					<Button
+						colorStyle="Success"
+						id="ss"
+						type="button"
+						onClick={() => setIsOpenFull(false)}
+					>
+						Close Full
+					</Button>
+				</div>
+			</Drawer>
+			<Drawer
+				openFrom="top"
+				disableClickOnBackdrop
+				percentage={90}
+				isOpen={isOpenTop}
+				setIsOpen={e => setIsOpenTop(e)}
+			>
+				<div style={{ backgroundColor: 'black', height: '100%' }}>
+					<Button
+						colorStyle="Success"
+						id="ss"
+						type="button"
+						onClick={() => setIsOpenTop(false)}
+					>
+						Close Full
+					</Button>
+				</div>
+			</Drawer>
+		</div>
   )
 }
 
